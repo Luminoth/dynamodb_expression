2395033a755d1a6fea20d0eb5829b008ab27e9b4
Shane Lillie <ignignokterr@gmail.com>
2020-08-03 22:07:31 -0700

    Project impl

--- src/projection.rs
+++ src/projection.rs
@@ -1,4 +1,6 @@
-use crate::{ExpressionNode, NameBuilder, TreeBuilder};
+use anyhow::bail;
+
+use crate::{ExpressionNode, NameBuilder, OperandBuilder, TreeBuilder};
 
 // https://github.com/aws/aws-sdk-go/blob/master/service/dynamodb/expression/projection.go
 
@@ -10,21 +12,40 @@ impl ProjectionBuilder {
     pub fn add_names(self, names_list: impl Into<Vec<Box<NameBuilder>>>) -> ProjectionBuilder {
         add_names(self, names_list)
     }
+
+    fn build_child_nodes(&self) -> anyhow::Result<Vec<ExpressionNode>> {
+        let mut child_nodes = Vec::new();
+        for name in &self.names {
+            let operand = name.build_operand()?;
+            child_nodes.push(operand.expression_node);
+        }
+        Ok(child_nodes)
+    }
 }
 
 impl TreeBuilder for ProjectionBuilder {
     fn build_tree(&self) -> anyhow::Result<ExpressionNode> {
-        unimplemented!("ProjectionBuilder::build_tree")
+        if self.names.len() == 0 {
+            bail!("ProjectionBuilder build_tree");
+        }
+
+        let child_nodes = self.build_child_nodes()?;
+
+        let mut node = ExpressionNode::from_children(child_nodes);
+        node.fmt_expression = format!("$c{}", ", $c".repeat(self.names.len() - 1));
+
+        Ok(node)
     }
 }
 
 pub fn names_list(
-    names_builder: Box<NameBuilder>,
+    name_builder: Box<NameBuilder>,
     names_list: impl Into<Vec<Box<NameBuilder>>>,
 ) -> ProjectionBuilder {
-    ProjectionBuilder {
-        names: names_list.into(),
-    }
+    let mut names_list = names_list.into();
+
+    names_list.insert(0, name_builder);
+    ProjectionBuilder { names: names_list }
 }
 
 pub fn add_names(
